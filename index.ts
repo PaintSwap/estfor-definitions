8f48bf8ac9ca78703619290117ac39bd44fd9b19
0xSamWitch <julian.assange12@outlook.com>
2023-03-13 21:23:26 +0000

    Convert to assemblyscript
    Co-authored-by: Justin Silver <jsilver@doublesharp.com>

---
+++ index.ts
@@ -0,0 +1,4 @@
+export { default as NONE } from './src/lib/estforConstants'
+import * as EstforTypes from './src/lib/estforTypes'
+import * as EstforConstants from './src/lib/estforConstants'
+export { EstforTypes, EstforConstants }
