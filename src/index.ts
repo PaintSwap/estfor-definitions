6ae8bee337aa49b7f9a7e4fa979870f7199e70dc
Justin Silver <jsilver@doublesharp.com>
2023-03-13 18:47:18 -0700

    estfor-*

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 export { default as NONE } from './constants'
 export * as EstforTypes from './types'
-export * as types from './types'
 export * as EstforConstants from './constants'
-export * as constants from './constants'
 
 
