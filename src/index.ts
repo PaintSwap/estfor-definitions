e75d2c6d4ede62acd48b37f07ae829b6ed813beb
Justin Silver <jsilver@doublesharp.com>
2023-03-13 18:18:30 -0700

    refactor to build to project base
    makes it easier to import specific items
    `import {HEAD_BASE} from '@paintswap/estfor-definitions/constants';`
    `import {Skill} from '@paintswap/estfor-definitions/types';`

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
-export { default as NONE } from './lib/estforConstants'
-import * as EstforTypes from './lib/estforTypes'
-import * as EstforConstants from './lib/estforConstants'
-export { EstforTypes, EstforConstants }
+export { default as NONE } from './constants'
+export * as EstforTypes from './types'
+export * as types from './types'
+export * as EstforConstants from './constants'
+export * as constants from './constants'
+
+
