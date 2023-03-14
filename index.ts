4bd58c0c1a5925d6e5d99052aaa5ffd5db2ea2ed
Justin Silver <jsilver@doublesharp.com>
2023-03-13 17:09:23 -0700

    build type files for assemblyscript

--- index.ts
+++
@@ -1,4 +0,0 @@
-export { default as NONE } from './src/lib/estforConstants'
-import * as EstforTypes from './src/lib/estforTypes'
-import * as EstforConstants from './src/lib/estforConstants'
-export { EstforTypes, EstforConstants }
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { default as NONE } from './lib/estforConstants'
+import * as EstforTypes from './lib/estforTypes'
+import * as EstforConstants from './lib/estforConstants'
+export { EstforTypes, EstforConstants }
