4b03e1dced7c5d3785d14d0bc0d0369dbea8d4aa
Justin Silver <jsilver@doublesharp.com>
2023-03-14 08:19:38 -0700

    use node 14 syntax

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-export { default as NONE } from './constants'
+import { default as NONE } from './constants'
 import * as EstforTypes from './types'
 import * as EstforConstants from './constants'
-export { EstforTypes, EstforConstants }
+
+export { EstforTypes, EstforConstants, NONE }
