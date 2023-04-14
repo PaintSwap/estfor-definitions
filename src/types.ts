e63778005000e6cb525d2ab4ac2ebc58d2f72610
0xSamWitch <julian.assange12@outlook.com>
2023-04-14 10:05:47 +0100

    Add player to ClanMember

--- src/types.ts
+++ src/types.ts
@@ -161,34 +161,6 @@ export const emptyNonCombatStats = new NonCombatStats()
 export const defaultInputItem = new InputItem()
 export const noAttire = new Attire()
 
-export class EquipmentInfo {
-  actionId: u16 = 0
-  queueId: string = '0'
-  elapsedTime: u32 = 0
-  itemTokenId: u16 = 0
-  amount: u32 = 0
-}
-
-export class XPInfo {
-  actionId: u16 = 0
-  queueId: string = '0'
-  elapsedTime: u32 = 0
-  xp: u32 = 0
-}
-
-export class DiedInfo {
-  actionId: u16 = 0
-  queueId: string = '0'
-  elapsedTime: u32 = 0
-}
-
-export class RollInfo {
-  actionId: u16 = 0
-  queueId: string = '0'
-  elapsedTime: u32 = 0
-  numRolls: number = 0
-}
-
 export class PastRandomRewardInfo {
   actionId: u16 = 0
   queueId: string = '0'
@@ -196,17 +168,28 @@ export class PastRandomRewardInfo {
   amount: u32 = 0
 }
 
+export class PendingQueuedActionEquipmentState {
+  consumed: Equipment[] = []
+  produced: Equipment[] = []
+}
+
+export class PendingQueuedActionMetadata {
+  xpGained: u32 = 0
+  rolls: u32 = 0
+  died: bool = false
+  actionId: u16 = 0
+  queueId: u64 = 0
+  elapsedTime: u32 = 0
+}
+
 export class PendingQueuedActionState {
-  consumed: EquipmentInfo[] = []
-  produced: EquipmentInfo[] = []
+  equipmentStates: PendingQueuedActionEquipmentState[] = []
+  actionMetadatas: PendingQueuedActionMetadata[] = []
   producedPastRandomRewards: PastRandomRewardInfo[] = []
   producedXPRewards: Equipment[] = []
   questRewards: Equipment[] = []
   questConsumed: Equipment[] = []
   activeQuestInfo: PlayerQuestOutput[] = []
-  died: DiedInfo[] = []
-  rolls: RollInfo[] = []
-  xpGained: XPInfo[] = []
   dailyRewards: Equipment[] = []
 }
 
@@ -580,6 +563,7 @@ export class Clan {
 
 export class ClanMember {
   id: string = '' // playerId
+  player: Player = new Player()
   clan: Clan = new Clan()
   requestedClan: Clan = new Clan()
   status: ClanStatus = ClanStatus.NOT_MEMBER
