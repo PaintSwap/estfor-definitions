5f07449575112a566e291a09dda4371bee9dcc6f
Justin Silver <jsilver@doublesharp.com>
2024-01-02 12:26:18 -0800

    use local dir

--- src/index.mjs
+++ src/index.mjs
@@ -1,5 +1,5 @@
-import { NONE } from '../constants.js'
-import * as EstforTypes from '../types.js'
-import * as EstforConstants from '../constants.js'
+import { NONE } from './constants.js'
+import * as EstforTypes from './types.js'
+import * as EstforConstants from './constants.js'
 
 export { EstforTypes, EstforConstants, NONE }
