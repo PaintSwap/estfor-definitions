b3d449b14d6f63fc9e87da18a3c0dab4db2171b1
0xSamWitch <julian.assange12@outlook.com>
2024-02-15 20:00:13 +0000

    Rename lastTradeTimestamp to lastTradedTimestamp in Order

--- src/types.ts
+++ src/types.ts
@@ -1284,7 +1284,7 @@ export class Order {
   tokenAmountClaimable: string = '0' // Brush amount that is still unclaimed
   nftAmountClaimable: string = '0' // NFT amount that is still unclaimed
   timestamp: string = '0'
-  lastTradeTimestamp: string = '0'
+  lastTradedTimestamp: string = '0'
 }
 
 export class TokenInfo {
