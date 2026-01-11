fee0a2ac2ea7abc2f1211e7818fe3ba8d3f26bfa
paintoshi <88572775+paintoshi@users.noreply.github.com>
2026-01-11 19:09:26 +0100

    fix: Cosmetic naming

--- src/types.ts
+++ src/types.ts
@@ -534,8 +534,8 @@ export class Player {
   soldHistory: PlayerSoldHistory[] = []
 
   /* Cosmetics */
-  avatarCosmeticId: CosmeticItem | null = null
-  avatarBorderCosmeticId: CosmeticItem | null = null
+  avatarCosmetic: CosmeticItem | null = null
+  avatarBorderCosmetic: CosmeticItem | null = null
 }
 
 export class PlayerSimplified {
@@ -605,8 +605,8 @@ export class PlayerSimplified {
   listingPrice: string = '0'
 
   /* Cosmetics */
-  avatarCosmeticId: CosmeticItem | null = null
-  avatarBorderCosmeticId: CosmeticItem | null = null
+  avatarCosmetic: CosmeticItem | null = null
+  avatarBorderCosmetic: CosmeticItem | null = null
 }
 
 export class User {
